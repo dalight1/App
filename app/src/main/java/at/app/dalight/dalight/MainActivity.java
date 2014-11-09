7630bd04a9e81e5ce54f8a2bedddcd4485a67b75
Stephan <stephan.rothschaedl@gmail.com>
2014-11-09 18:39:20 +0100

    simple TextView

--- app/src/main/java/at/app/dalight/dalight/MainActivity.java
+++ app/src/main/java/at/app/dalight/dalight/MainActivity.java
@@ -6,7 +6,14 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -15,10 +22,18 @@ public class MainActivity extends Activity {
     private FloatingActionButton addButton;
     private Boolean mode;
 
+    //List view: {views: items.xml}
+    private ListView list;
+
+    //Extended ListView
+    private List<Device> myDevices = new ArrayList<Device>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.listViewMain);
 
         addButton = new FloatingActionButton.Builder(this)
                 .withDrawable(getResources().getDrawable(R.drawable.ic_add))
@@ -35,7 +50,13 @@ public class MainActivity extends Activity {
         });
         mode = false;
 
+        populateListView();
+        registerClickCallback();
+
     }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -71,4 +92,36 @@ public class MainActivity extends Activity {
         }
     }
 
+
+
+    private void populateListView(){
+        //Create a List of Items
+        String[] myitems = {"eins", "zwei", "drei"};
+
+        //Build Adapter
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,           //Context for the activity
+                R.layout.items, //Layout to use (create)
+                myitems);       //Items to be displayed
+
+        //Configure the ListView
+        //ListView list = (ListView) findViewById(R.id.listViewMain);
+        list.setAdapter(adapter);
+
+    }
+    private void registerClickCallback() {
+        //ListView list = (ListView) findViewById(R.id.listViewMain);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = (TextView) view;
+                String message = "You Clicked " +
+                        position +
+                        " which ist string " +
+                        textView.getText().toString();
+                Toast.makeText(MainActivity.this, message,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
