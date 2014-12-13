c0b59883e85e0886b18fcaa91de75852d9ed4288
Stephan <stephan.rothschaedl@gmail.com>
2014-12-13 12:00:41 +0100

    add Group and Scene
    Groups können ausgelesen werden Mittels "getGroups()"

--- app/src/main/java/at/app/dalight/dalight/Device.java
+++ app/src/main/java/at/app/dalight/dalight/Device.java
@@ -1,5 +1,7 @@
 package at.app.dalight.dalight;
 
+import java.util.ArrayList;
+
 /**
  * Created by Stephan on 09.11.2014.
  * This class represents one Device at the DALI Bus
@@ -9,6 +11,8 @@ public class Device {
     private String name;
     private int iconId;
     private int adress;
+    private boolean[] group;
+    private boolean[] scene;
     private String type;
 
 
@@ -24,6 +28,14 @@ public class Device {
         this.iconId = iconId;
         this.adress = adress;
         this.type = "invalid Type";
+    }
+
+    public ArrayList<Integer> getGroups(){
+        ArrayList<Integer> usedGroups = new ArrayList();
+        for (int groupIdx = 0;groupIdx < this.group.length;groupIdx++){
+            if (this.group[groupIdx]) usedGroups.add(groupIdx);
+        }
+        return usedGroups;
     }
 
     public String getName() {
